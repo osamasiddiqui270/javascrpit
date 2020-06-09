7117b89fd3aaf28d79c5cadeb16db09e825a2e53
osamasiddiqui270 <66634471+osamasiddiqui270@users.noreply.github.com>
2020-06-10 00:03:02 +0530

    completed my first project

--- vanilla project/js/script.js
+++ vanilla project/js/script.js
@@ -12,10 +12,15 @@ function showerror(input,postMessage) {
 	small.innerText = postMessage;
 }
 //check email added
-function checkemail(){
+function checkemail(input){
 	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())){
+		showsuccess(input);
+	}else{
+		showerror(input, 'Email is not valid');
+	}
 }
+	
 //show success outline
 function showsuccess(input,postMessage){
 const formControl = input.parentElement;
@@ -31,6 +36,24 @@ function checkrequired(inputArr){
 		}
 	});
 }
+//check password match
+function checkpassword(input1, input2){
+	if(input1.value !== input2.value){
+		showerror(input2, 'Password do not match');
+	}
+	
+}
+//chech lenght
+ function checklenght(input, min, max){
+	 if(input.value.length < min) {
+		 showerror(input, `${getFieldName(input)} must be at least ${min}characters`);
+	 } else if(input.value.lenght > max){
+		  showerror(input,`${getFieldName(input)} must be less than ${max}characters`);
+	 }else{
+		 showsuccess(input);
+	 }
+ }
+
 //get field name
 function getFieldName(input){
 	return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -41,7 +64,9 @@ form.addEventListener('submit',function(e){
 	e.preventDefault();
 
 	 checkrequired([username,email,password,password2]);
-	
-	
+	 checklenght(username, 3, 15);
+	 checklenght(password, 6, 25);
+     checkemail(email);
+	 checkpassword(password, password2);
 });
 
