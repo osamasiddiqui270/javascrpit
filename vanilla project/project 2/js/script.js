451bd97cfe9f5e811cb358c2fc58a103e5c671e5
osamasiddiqui270 <66634471+osamasiddiqui270@users.noreply.github.com>
2020-06-12 23:47:04 +0530

    count and total reading done

--- vanilla project/project 2/js/script.js
+++ vanilla project/project 2/js/script.js
@@ -5,8 +5,28 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieselect = document.getElementById('movie');
 
-const ticketprice = +movieselect.value;
+let ticketprice = +movieselect.value;
 
+//update totak and count
+
+function updateselectedcount(){
+const selectedseats	= document.querySelectorAll('.row .seat.selected');
+	
+const selectedseatscount = selectedseats.length;
+	
+count.innerText = selectedseatscount;
+total.innerText = selectedseatscount * ticketprice;	
+}
+
+//movie select
+movieselect.addEventListener('change', function(e){
+	ticketprice = +e.target.value;
+	updateselectedcount();
+})
+
+
+
+// seat 
 container.addEventListener ('click', function(e){
 	if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
 		e.target.classList.toggle('selected');
