14bb5fea6aba4bce33d4f610004e99bf5c1cd370
osamasiddiqui270 <66634471+osamasiddiqui270@users.noreply.github.com>
2020-06-15 22:18:28 +0530

    get the data from local storage

--- vanilla project/project 2/js/script.js
+++ vanilla project/project 2/js/script.js
@@ -5,7 +5,26 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieselect = document.getElementById('movie');
 
+populateUI;
+
 let ticketprice = +movieselect.value;
+// save movie seat and price
+function setmoviedata(movieindex, movieprice){
+	localStorage.setItem('selectedmovieindex', movieindex);
+	localStorage.setItem('selectedmovieprice', movieprice);
+}
+// get data from localstorage
+function populateUI() {
+const selectedseats = JSON.parse(localStorage.getItem('selectedseats'));
+if(selectedseats !== null && selectedseats.length > 0){
+	seats.forEach((seat, index) => {
+	if(selectedseats.indexOf(index) > -1) {
+	   seat.classList.add('selected');
+	   }	
+	});
+ }	
+}
+
 
 //update totak and count
 
@@ -14,7 +33,8 @@ const selectedseats	= document.querySelectorAll('.row .seat.selected');
 
 	const seatsindex = [...selectedseats].map(seat => [...seats].indexOf(seat));
 	
-	localStorage.setItem('selectedseats',JSON.stringify(seatsindex));
+	
+	localStorage.setItem('selectedseats', JSON.stringify(seatsindex));
 	
 	
 const selectedseatscount = selectedseats.length;
